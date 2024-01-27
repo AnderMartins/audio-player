4d605961b2a4f9e277fa9550d60a6a7e62f916dc
Anderson Martins <martins_xd@yahoo.com.br>
2024-01-27 08:48:25 -0300

    First Commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "Granite",
+    artist: "Sleep Token",
+    cover: "granite.jpg",
+    file: "granite.mp3"
+  },
+  {
+    title: "Chokehold",
+    artist: "Sleep Token",
+    cover: "chokehold.jpg",
+    file: "chokehold.mp3"
+  },
+  {
+    title: "Take Me Back To Eden",
+    artist: "Sleep Token",
+    cover: "take-me-back-to-eden.jpg",
+    file: "take-me-back-to-eden.mp3"
+  },
+  {
+    title: "The Summoning",
+    artist: "Sleep Token",
+    cover: "the-summoning.jpg",
+    file: "the-summoning.mp3"
+  }
+];
---
+++ player.js
@@ -0,0 +1,90 @@
+import audios from "./data.js";
+import { path, secondsToMinutes } from "./utils.js";
+import elements from "./playerElements.js";
+
+export default {
+    audioData: audios,
+    currentAudio: {},
+    currentPlaying: 0,
+    isPlaying: false,
+    start() {
+        elements.get.call(this);
+        this.update();
+    },
+    play() {
+        this.isPlaying = true;
+        this.audio.play();
+        this.playPause.classList.remove('mdi-play');
+        this.playPause.classList.add('mdi-pause');
+    },
+    pause() {
+        this.isPlaying = false;
+        this.audio.pause();
+        this.playPause.classList.remove('mdi-pause');
+        this.playPause.classList.add('mdi-play');
+    },
+    togglePlayPause() {
+        if(this.isPlaying) {
+            this.pause();
+        } else {
+            this.play();
+        }
+    },
+    toggleMute() {
+        this.audio.muted = !this.audio.muted;
+        this.mute.classList.remove('mdi-volume-high');
+        this.mute.classList.remove('mdi-volume-mute');
+        this.mute.classList.add(this.audio.muted ? 'mdi-volume-mute' : 'mdi-volume-high');
+    },
+    next() {
+        this.currentPlaying++;
+        if (this.currentPlaying == this.audioData.length) {
+            this.currentPlaying = 0;
+        }
+        this.update();
+        this.play();
+    },
+    setVolume(value) {
+        this.audio.volume = value / 100;
+    },
+    setSeek(value) {
+        this.audio.currentTime = value;
+    },
+    timeUpdate() {
+        this.currentDuration.innerText = secondsToMinutes(this.audio.currentTime);
+        this.seekbar.value = this.audio.currentTime;
+    },
+    update() {
+        this.currentAudio = this.audioData[this.currentPlaying];
+        this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`;
+        this.title.innerHTML = `<i class="mdi mdi-music"</i> ${this.currentAudio.title}`;
+        this.artist.innerHTML = `<i class="mdi mdi-account"</i> ${this.currentAudio.artist}`;
+        elements.createAudioElement.call(this, path(this.currentAudio.file));
+        this.audio.onloadeddata = () => {
+            elements.actions.call(this);
+        }
+        
+    },
+    restart() {
+        this.currentPlaying = 0;
+        this.update();
+    },
+    antes() {
+        this.pause();
+        this.currentPlaying--;
+        if (this.currentPlaying < 0) {
+            this.currentPlaying = this.audioData.length - 1;
+        }
+        this.update();
+        this.play();
+    },
+    depois() {
+        this.pause();
+        this.currentPlaying++;
+        if (this.currentPlaying == this.audioData.length) {
+            this.currentPlaying = 0;
+        }
+        this.update();
+        this.play();
+    },
+};
