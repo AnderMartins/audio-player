b952bb14cffd8906bbf4acc1ecfadad16da0ce08
Anderson Martins <martins_xd@yahoo.com.br>
2024-02-06 15:11:50 -0300

    Ajuste de uma linha tirada por engano!

--- player.js
+++ player.js
@@ -15,7 +15,7 @@ export default {
         if (isMobile) {
             document.querySelector('.icone-volume').style.display = 'none';
             document.querySelector('#vol-control').style.display = 'none';
-            document.querySelector('#current-duration').style.marginLeft = '25px';
+            document.querySelector('#current-duration').style.marginLeft = '30px';
             document.querySelector('#total-duration').className = 'col s1';
         }
 
@@ -27,6 +27,8 @@ export default {
                 this.play();
             }
         });
+        
+        this.update();
     },
     play() {
         this.isPlaying = true;
