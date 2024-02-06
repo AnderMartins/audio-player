09fd47014ed93266ffe2432a190b94163527f0eb
Anderson Martins <martins_xd@yahoo.com.br>
2024-02-06 12:27:54 -0300

    AjusteLayout volume

--- player.js
+++ player.js
@@ -15,6 +15,8 @@ export default {
         if (isMobile) {
             document.querySelector('.icone-volume').style.display = 'none';
             document.querySelector('#vol-control').style.display = 'none';
+            document.querySelector('#current-duration').style.marginLeft = '35px';
+            document.querySelector('#total-duration').style.marginRight = '-35px';
         }
 
         this.playPause.addEventListener('touchend', (event) => {
