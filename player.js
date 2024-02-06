2c12069d82c350a0cad30b05ae7217adf1d8146f
Anderson Martins <martins_xd@yahoo.com.br>
2024-02-06 14:13:41 -0300

    AjusteLayout volume 12

--- player.js
+++ player.js
@@ -15,7 +15,7 @@ export default {
         if (isMobile) {
             document.querySelector('.icone-volume').style.display = 'none';
             document.querySelector('#vol-control').style.display = 'none';
-            document.querySelector('#current-duration').style.marginLeft = '45px';
+            document.querySelector('#current-duration').style.marginLeft = '35px';
             document.querySelector('#total-duration').className = 'col s1';
         }
 
